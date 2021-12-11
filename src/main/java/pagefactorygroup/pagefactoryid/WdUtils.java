8f5d7e8aef5410ff1569444ae7db2e0272b15785
mohitchoudhary747 <mohitchoudhary1@outlook.com>
2021-12-11 11:05:27 +0530

    First Commit for my pom framework project

---
+++ src/main/java/pagefactorygroup/pagefactoryid/WdUtils.java
@@ -0,0 +1,19 @@
+package pagefactorygroup.pagefactoryid;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WdUtils {
+	
+	public static WebElement waitForElementToVisible(WebDriver localDriver,WebElement element)
+	{
+		WebDriverWait wait=new  WebDriverWait(localDriver, Duration.ofSeconds(10000));
+		return wait.until(ExpectedConditions.visibilityOf(element));
+		
+	}
+
+}
